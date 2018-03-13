bc91c577ff5a996147e500d9c039335c6ad04333
Felipe1994 <luizfelipesousa@hotmail.com>
2018-03-13 10:51:57 -0300

    teste

--- features/environment.py
+++ features/environment.py
@@ -6,6 +6,6 @@ from behave import *
 def after_all(context):
     with open('./report/report.json') as behave_json:
         cucumber_json = behave2cucumber.convert(json.load(behave_json))
-        arquivo = open('.resultado-teste.json','w')                
+        arquivo = open('resultado-teste.json','w')                
         arquivo.write(str(cucumber_json).replace("'",'"'))
         arquivo.close()
